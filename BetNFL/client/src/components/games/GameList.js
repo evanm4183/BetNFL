f812741ebf6840a76ddc1aa3ed4d7d7fcbcc7b8f
Evan Mitchell <evanm4183@gmail.com>
2022-10-24 15:11:35 -0500

    Users can view all the games in a week

--- BetNFL/client/src/components/games/GameList.js
+++ BetNFL/client/src/components/games/GameList.js
@@ -1,7 +1,28 @@
 import { useState, useEffect } from "react";
+import GameCard from "./GameCard";
+import "../../styles/game-styles.css";
+import { getSiteTime } from "../../modules/siteTimeManager";
+import { getGamesByWeek } from "../../modules/gameManager";
 
 export default function GameList() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        getSiteTime().then((siteTime) => {
+            getGamesByWeek(siteTime.currentWeek).then((games) => {
+                setGames(games);
+            });
+        })
+    }, []);
+    
     return (
-        <div>Game List</div>
+        <>
+            <h4 className="game-list-header">Week {games[0]?.week}</h4>
+            <div className="game-list-container">
+                {
+                    games.map((game) => <GameCard key={game.id} game={game} />)
+                }
+            </div>
+        </>
     );
 }
--- BetNFL/client/src/modules/gameManager.js
+++ BetNFL/client/src/modules/gameManager.js
@@ -2,6 +2,19 @@ import { getToken } from "./authManager";
 
 const apiUrl = "api/game";
 
+export const getGamesByWeek = (week) => {
+    return getToken().then((token) => {
+        return fetch(`${apiUrl}/${week}`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+        }).then((res) => {
+            return res.json()
+        });
+    });
+}
+
 export const postGame = (game) => {
     return getToken().then((token) => {
         return fetch(apiUrl, {
